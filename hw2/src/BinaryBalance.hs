f7ef8794b40ec2e5be6b6941787e4395b6e7a0f0
ycdfwzy <ycdfwzy@outlook.com>
2019-03-12 23:03:23 +0800

    improve hw2

--- hw2/src/BinaryBalance.hs
+++ hw2/src/BinaryBalance.hs
@@ -1,8 +1,15 @@
 module BinaryBalance where
 
 import BinTree
-import BinaryHeight
 
 isBalance :: BinTree.BinTree a -> Bool
 isBalance Nil = True
-isBalance (Node leftNode rightNode a) = (isBalance leftNode) && (isBalance rightNode) && (abs ((height leftNode)-(height rightNode)) < 2)
+isBalance (Node leftNode rightNode a) = let (_, x)=checkBalance (Node leftNode rightNode a) in x
+    where
+        checkBalance Nil = (0, True)
+        checkBalance (Node leftNode rightNode a) = 
+            let (left_h, left_flag) = checkBalance leftNode in
+            let (right_h, right_flag) = checkBalance rightNode in
+            (max left_h right_h + 1, left_flag && right_flag && (abs (left_h-right_h) < 2))
+
+-- isBalance (Node leftNode rightNode a) = (isBalance leftNode) && (isBalance rightNode) && (abs ((height leftNode)-(height rightNode)) < 2)
--- hw2/src/GCD.hs
+++ hw2/src/GCD.hs
@@ -3,6 +3,10 @@ module GCD
     ) where
 
 myGCD :: Integer->Integer->Integer
-myGCD a 0 = abs a
-myGCD 0 b = abs b
-myGCD a b = abs (myGCD b (mod a b))
+myGCD a b = myGCD_ (abs a) (abs b)
+    where
+        myGCD_ a 0 = a
+        myGCD_ a b = myGCD_ b (rem a b)
+-- myGCD a 0 = abs a
+-- myGCD 0 b = abs b
+-- myGCD a b = abs (myGCD b (mod a b))
