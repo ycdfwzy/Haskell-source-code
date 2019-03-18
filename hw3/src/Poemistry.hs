c17ccf5f268c78bbe34cc1bc4cbd280290734cd7
ycdfwzy <ycdfwzy@outlook.com>
2019-03-18 23:06:47 +0800

    add hw2/prob3

--- hw3/src/Poemistry.hs
+++ hw3/src/Poemistry.hs
@@ -1,7 +1,24 @@
 module Poemistry where
 
-poemistry :: [Char] ->Integer-> [Char]
-poemistry = undefined
+poemistry :: [Char] -> Integer -> [Char]
+poemistry strlist k = let newstr = strlist in getPoemistry newstr k 0
+
 
 prettyPrint :: [Char] -> [Char]
-prettyPrint = undefined
+prettyPrint [] = []
+prettyPrint (x1:x2:x3:x4:x5:xs) = (x1:x2:x3:x4:x5:'\n':(prettyPrint xs))
+
+-- sort :: Ord a => [a] -> [a]
+-- sort [] = []
+-- sort (x:xs) = sort [y | y <- xs, y <= x] ++ [x] ++ sort [y | y <- xs, y > x]
+
+getPoemistry :: [Char] -> Integer -> Integer -> [Char]
+getPoemistry strlist k i | i < 20 = let w = toInteger $ (length strlist)^(19-i) in
+                                        ((getCharInStr strlist (k `div` w)) : (getPoemistry strlist (k `mod` w) (i+1)))
+                         | otherwise = []
+
+getCharInStr :: [Char] -> Integer -> Char
+getCharInStr [] k = error "string is empty!"
+getCharInStr (c:cs) k | k == 0 = c
+                 | k > 0 = getCharInStr cs (k-1)
+                 | otherwise = error "k < 0!"
