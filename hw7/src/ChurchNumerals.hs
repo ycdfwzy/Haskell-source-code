bb157b26e015905d9e75d2f3fa75f0cf40bb42b2
ycdfwzy <ycdfwzy@outlook.com>
2019-04-20 15:23:43 +0800

    update hw7

--- hw7/src/ChurchNumerals.hs
+++ hw7/src/ChurchNumerals.hs
@@ -9,5 +9,7 @@ cToInt cn = cn (+1) 0
 cPlus cn cm= \f -> \x -> cm f (cn f x)
 
 cMult cn cm = cn . cm
+cMult' cn cm = \f -> \x -> cn (cm f) x
 
 cExp cn cm = cm $ cn
+cExp' cn cm = \f -> \x -> (cm cn f) x
